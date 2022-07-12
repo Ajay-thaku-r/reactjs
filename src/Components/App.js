4a906434857778968f4d049d0f734c8eb6644b21
Ajay Kumar <ajaythakur9131@gmail.com>
2022-07-12 16:45:48 +0530

    basic commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 import './css/App.css';
 import Header from './common/Header';
 import Footer from './common/Footer';
@@ -7,11 +8,16 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 function App() {
+  const [users, setUsers] = useState([]);
+  const addUserHandler = (user) => {
+    setUsers([...users, {id: uuid(), ...user}])
+  }
+  console.log(users)
   return (
     <div>
         <Header />
           <Routes>
-            <Route path="/" element={ <Home /> }></Route>
+            <Route addUserHandler={ addUserHandler } path="/" element={ <Home /> } ></Route>
             <Route path="/about" element={ <About /> }></Route>
             <Route path="/contact" element={ <Contact /> }></Route>
           </Routes>
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-const Home = () => {
+class Home extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+     addData = (e) => {
+        e.preventDefault()
+        if(this.state.username === "" || this.state.password === ""){
+            alert("All Fields Are Mandatory")
+            return;
+        }
+        this.props.addUserHandler(this.state)
+        this.setState({username:"",password:""})
+    }
+   
+    render(){
     return (
         <div>
             <div className="container">
                 <div className="row d-flex align-items-center" style={{height:"70vh"}}>
                     <div className="col d-flex justify-content-center">
                         <div className="col-3 col-form">
-                            <form className="text-center the-form">
-                                <input type="text" name="username" value="" placeholder="Set UserName" className="form-input"/><br></br><br></br>
-                                <input type="password" name="password" value="" placeholder="Set password" className="form-input"/><br></br><br></br>
+                            <form onSubmit={ this.addData } className="text-center the-form">
+                                <input type="text" name="username" value={this.state.username} onChange={(e)=>this.setState({username: e.target.value})}placeholder="Set UserName" className="form-input"/><br></br>
+                                Your Username is {this.state.username}
+                                <br></br><br></br>
+                                <input type="password" name="password" value={this.state.password} onChange={(e)=>this.setState({password:e.target.value})} placeholder="Set password" className="form-input"/><br></br>
+                                <br></br>
                                 <input type="submit" value="Sign Up" className="btn"/>
                             </form>
                         </div>
@@ -18,5 +36,6 @@ const Home = () => {
             </div>
         </div>
     )
+    }
 }
 export default Home;
