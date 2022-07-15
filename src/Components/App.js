4e168ccf416907200bd561fc06a5eee3a710276d
Ajay Kumar <62602303+Ajay-thaku-r@users.noreply.github.com>
2022-07-15 12:23:16 +0530

    use memo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import './css/App.css';
@@ -7,7 +7,10 @@ import Footer from './common/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Factorial from './pages/Factorial';
 function App() {
+  // const [counter, setCounter] = useState(1);
+  // const result = factorial(counter);
   const [users, setUsers] = useState([]);
   const addUserHandler = (user) => {
     setUsers([...users, {id: uuid(), ...user}])
@@ -17,9 +20,10 @@ function App() {
     <div>
         <Header />
           <Routes>
-            <Route addUserHandler={ addUserHandler } path="/" element={ <Home /> } ></Route>
+            <Route path="/" element={ <Home addUserHandler={ addUserHandler } /> } ></Route>
             <Route path="/about" element={ <About /> }></Route>
             <Route path="/contact" element={ <Contact /> }></Route>
+            <Route path="/factorial" element={ <Factorial /> }></Route>
           </Routes>
         <Footer />
     </div>
---
+++ src/Components/pages/Factorial.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Factorial = () => {
+    return (
+        <>
+            coming in factoial
+        </>
+    )
+}
+export default Factorial;
